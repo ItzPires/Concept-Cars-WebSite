c82855faafdd6c39cd55ff8b7d7424d31f30beda
Pedro Henriques <pedroduartesh@gmail.com>
2023-10-24 19:11:31 +0100

    React project init

---
+++ concept-drives/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NavBar from './component/NavBar.js';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ concept-drives/src/component/NavBar.js
@@ -0,0 +1,14 @@
+// Desc: NavBar component
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+
+    return (
+        <h1>CONCEPT DRIVES</h1>
+    );
+
+
+}
+
+export default NavBar;

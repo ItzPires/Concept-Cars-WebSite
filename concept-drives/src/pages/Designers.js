07543487d43115d04374f14170ddf3bec211e7ac
Samuel Pires <spires@ipn.pt>
2023-11-03 23:09:54 +0000

    Add grid layout for designers page

--- concept-drives/src/pages/Designers.js
+++ concept-drives/src/pages/Designers.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Designers.css';
 
 const Designers = () => {
-  return (
-    <h1>sss</h1>
-  );
+    return (
+        <div class="context">
+            <div class="col1">1</div>
+            <div class="col2">2</div>
+            <div class="col3">3</div>
+            <div class="col4">4</div>
+            <div class="col5">5</div>
+            <div class="col6">6</div>
+            <div class="col7">7</div>
+            <div class="col8">8</div>
+            <div class="col9">9</div>
+            <div class="col10">10</div>
+            <div class="col11">11</div>
+            <div class="col12">12</div>
+        </div>
+    );
 };
 
 export default Designers;

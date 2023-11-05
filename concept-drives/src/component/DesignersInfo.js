3b20e7b92fb8e902369f56dff453c2d0880dbc05
Samuel Pires <spires@ipn.pt>
2023-11-05 20:00:33 +0000

    Refactor DesignersInfo component to improve readability and maintainability

--- concept-drives/src/component/DesignersInfo.js
+++ concept-drives/src/component/DesignersInfo.js
@@ -4,31 +4,41 @@ import '../pages/Designers.css';
 function DesignerInfo({ designer, index }) {
   const isEven = index % 2 === 0;
 
+  const returnImage = (
+    <>
+      <img src={designer.imageSrc} alt={designer.name} className="img-designers" />
+    </>
+  );
+
+  const returnText = (
+    <>
+      <div>
+        <h2>{designer.name}</h2>
+        <p>{designer.description}</p>
+      </div>
+    </>
+  );
+
+  const renderContent = (first) => {
+    if (isEven && first) {
+      return returnImage;
+    } else if (isEven) {
+      return returnText;
+    }
+    else if (first) {
+      return returnText;
+    } else {
+      return returnImage;
+    }
+  };
+
   return (
     <div className="context">
-      <div className="col-3-6">
-        <div className="content-wrapper">
-          {isEven ? (
-            <img src={designer.imageSrc} alt={designer.name} className="img-designers" />
-          ) : (
-            <>
-              <h2>{designer.name}</h2>
-              <p>{designer.description}</p>
-            </>
-          )}
-        </div>
+      <div className={`col-3-6`}>
+        <div className="content-wrapper">{renderContent(true)}</div>
       </div>
-      <div className="col-7-10">
-        <div className="content-wrapper">
-          {isEven ? (
-            <>
-              <h2>{designer.name}</h2>
-              <p>{designer.description}</p>
-            </>
-          ) : (
-            <img src={designer.imageSrc} alt={designer.name} className="img-designers" />
-          )}
-        </div>
+      <div className={`col-7-10`}>
+        <div className="content-wrapper">{renderContent(false)}</div>
       </div>
     </div>
   );

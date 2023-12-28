4e00caa61c990f6a30647f9517c8ff311dbbc1d0
Samuel Pires <spires@ipn.pt>
2023-12-28 00:11:57 +0000

    Add Title to Designers Page

--- concept-drives/src/pages/Designers.js
+++ concept-drives/src/pages/Designers.js
@@ -2,42 +2,42 @@ import React, { useState, useEffect } from "react";
 import DesignerInfo from "../component/DesignersInfo/DesignersInfo";
 import Designer from "../class/Designer";
 import Loading from "../component/Loading/Loading";
-import "./Designers.css";
 import APIService from "../APIService";
+import "./Designers.css";
+
 const Designers = () => {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
-  function timeoutDelay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+    useEffect(() => {
 
-  useEffect(() => {
-    
-    APIService.getDesignerList().then((res) => {
-      const designersData = res.objects.map((designer) => {
-        return new Designer(
-          designer.metadata.image.url,
-          designer.title,
-          designer.metadata.description
-        );
-      });
-      setData(designersData);
-    });
-  }, []);
+        APIService.getDesignerList().then((res) => {
+            const designersData = res.objects.map((designer) => {
+                return new Designer(
+                    designer.metadata.image.url,
+                    designer.title,
+                    designer.metadata.description
+                );
+            });
+            setData(designersData);
+        });
+    }, []);
 
-  return (
-    <>
-      {data ? (
-        <div className="context">
-          {data.map((designer, index) => (
-            <DesignerInfo key={index} designer={designer} index={index} />
-          ))}
-        </div>
-      ) : (
-        <Loading />
-      )}
-    </>
-  );
+    return (
+        <>
+            <div className="cars_header">
+                <h1 className="cars_title">Designers</h1>
+            </div>
+            {data ? (
+                <div className="context">
+                    {data.map((designer, index) => (
+                        <DesignerInfo key={index} designer={designer} index={index} />
+                    ))}
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
+    );
 };
 
 export default Designers;
